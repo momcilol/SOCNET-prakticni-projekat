de601f724984991acf1cd87c2d58ffa8c87fbd9b
momcilo <milosh.maric.90@gmail.com>
2022-08-20 23:43:29 +0200

    testing started

--- vezbe/pom.py
+++ vezbe/pom.py
@@ -2,6 +2,9 @@ import graphlib
 import networkx as nx
 
 
-graph = nx.Graph()
-graph.name = "moj graf"
-print(graph.name)
+# graph = nx.Graph()
+# graph.name = "moj graf"
+# print(graph.name)
+
+str = input("Unesite: ")
+print(str.replace("{", "(").replace("}", ", {'sign': '+'})"))
